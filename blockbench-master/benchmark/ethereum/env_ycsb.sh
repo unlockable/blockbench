39231aef82dfe000785bd5151c004f510853e824
hwang <unlockable@naver.com>
2019-02-07 23:16:47 +0900

    Initial commit

---
+++ blockbench-master/benchmark/ethereum/env_ycsb.sh
@@ -0,0 +1,11 @@
+ETH_HOME=/home/hwang/Blockbench/blockbench-master/benchmark/ethereum
+HOSTS=$ETH_HOME/hosts
+CLIENTS=$ETH_HOME/clients
+ETH_DATA=$ETH_HOME/ether_data
+LOG_DIR=$ETH_HOME/LOG_Result
+EXE_HOME=$ETH_HOME/../../src/ycsb
+BENCHMARK=ycsb
+
+##comment these out for smallbank
+#EXE_HOME=$ETH_HOME/../../src/smallbank/ethereum/ycsb
+#BENCHMARK=smallbank
