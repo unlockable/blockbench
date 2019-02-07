a4d3586deb6600b576a8f3d25940454c69584d62
hwang <unlockable@naver.com>
2019-02-07 23:21:45 +0900

    Initial commit

--- benchmark/ethereum/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#args: number_of_nodes
-cd `dirname ${BASH_SOURCE-$0}`
-. env.sh
-
-geth --datadir=$ETH_DATA init $ETH_HOME/CustomGenesis"_"$1".json"
-geth --datadir=$ETH_DATA --password <(echo -n "") account new
